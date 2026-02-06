de0bec69fb81a16a64b665585f67be1f614b75b5
db-0 <dan@dbsphere.net>
2026-02-06 09:52:10 -0700

    config management baseline - incorporating changes in the example solution files

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,22 +1,72 @@
 package config
 
 import (
+	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
 const configFileName = ".gatorconfig.json"
 
 type Config struct {
-	db_url            string `json:"db_url"`
-	current_user_name string `json:"current_user_name"`
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
 }
 
-func Read() Config {
-	configDir, err := os.UserHomeDir()
+func (cfg *Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
+	return write(*cfg)
+}
+
+func Read() (Config, error) {
+	fullPath, err := getConfigFilePath()
 	if err != nil {
-		println(err)
+		return Config{}, err
 	}
 
-	configFilePath := configDir + configFileName
+	file, err := os.Open(fullPath)
+	if err != nil {
+		return Config{}, err
+	}
 
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func getConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	fullPath := filepath.Join(home, configFileName)
+	return fullPath, nil
+}
+
+func write(cfg Config) error {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/db-0/gator/internal/config"
+)
+
 func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading configuration file: %v", err)
+	}
+	fmt.Printf("Read configuration file: %+v\n", cfg)
+
+	err = cfg.SetUser("dan")
+	if err != nil {
+		log.Fatalf("couldn't set current user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading configuration file: %v", err)
+	}
+
+	fmt.Printf("Read configuration file again: %+v\n", cfg)
 }
